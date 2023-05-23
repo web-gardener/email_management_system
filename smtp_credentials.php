6535e75d9f5055687231695714ecd61ebcd6ad22
web-gardener <zhulishen0331@gmail.com>
2023-05-24 01:02:58 +0800

    Update smtp_credentials.php

--- smtp_credentials.php
+++ smtp_credentials.php
@@ -1,4 +1,4 @@
 <?php
-$smtpUsername = 'ikhwanm32@outlook.com'; //your ooutlook email
-$smtpPassword = '33221nawhkI@'; //your outlook password
+$smtpUsername = 'email@outlook.com'; //your ooutlook email
+$smtpPassword = 'password'; //your outlook password
 ?>
