d8569ee264eaa6412a8aebcd88226e2ce86d5f09
web-gardener <zhulishen0331@gmail.com>
2023-05-23 23:56:21 +0800

    Update imap_retrieve.php

--- imap_retrieve.php
+++ imap_retrieve.php
@@ -1,6 +1,6 @@
 <?php
 // Retrieve emails from Outlook.com using IMAP
 $imapPath = '{outlook.office365.com:993/imap/ssl}';
-$username = 'ikhwanm32@outlook.com';
-$password = '33221nawhkI@';
+$username = 'your_email@outlook.com';
+$password = 'password';
 ?>
